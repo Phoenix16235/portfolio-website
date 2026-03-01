ef6b111d2078352a58403fc484d1be460fd5838e
Phoenix16235 <r.broughton162@gmail.com>
2026-03-02 09:13:10 +1030

    Added contact section and smooth scroll from hero button

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,15 @@ console.log("JS is connected");
 const primarybutton = document.querySelector(".btn-primary");
 const projectsSection = document.querySelector("#projects");
 
-primarybutton.addEventListener("click", function(event) {
+primarybutton.addEventListener("click", function (event) {
     event.preventDefault();
     projectsSection.scrollIntoView({ behavior: "smooth" });
+});
+
+const contactbutton = document.querySelector(".btn-secondary");
+const contactsSection = document.querySelector("#contact");
+
+contactbutton.addEventListener("click", function (event) {
+    event.preventDefault();
+    contactsSection.scrollIntoView({ behavior: "smooth"});
 });
