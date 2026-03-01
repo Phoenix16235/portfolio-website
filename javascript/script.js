ba89188adcb0df51daafbccc8ce0b1a4c06eae3c
Phoenix16235 <r.broughton162@gmail.com>
2026-03-02 09:05:28 +1030

    Added ID'S to HTML sections, to add a smooth scroll from hero button to projects section

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
 // Portfolio JS will go here
+console.log("JS is connected");
+
+const primarybutton = document.querySelector(".btn-primary");
+const projectsSection = document.querySelector("#projects");
+
+primarybutton.addEventListener("click", function(event) {
+    event.preventDefault();
+    projectsSection.scrollIntoView({ behavior: "smooth" });
+});
